4d573d58efe52315ddb4d04456f6279685d0a9ea
schoetlr <srlrs20020@gmail.com>
2016-08-23 09:30:26 -0400

    response count

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,4 +2,10 @@ class Survey < ActiveRecord::Base
   has_many :questions
   
   validates :title, :description, presence: true
+
+  def response_count
+    question = self.questions.where(required: true).first
+
+    question.nil? ? 0 : question.answers.size
+  end
 end
